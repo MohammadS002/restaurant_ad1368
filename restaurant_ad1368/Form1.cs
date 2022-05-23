c65971d125fab64f29f4b570175e000d33ba0b1f
محمد علي <AD1368@iu.edu.jo>
2022-05-24 02:59:13 +0300

    access

--- restaurant_ad1368/Form1.cs
+++ restaurant_ad1368/Form1.cs
@@ -7,28 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace restaurant_ad1368
 {
     public partial class frmlogin : Form
     {
+        private OleDbConnection conn = new OleDbConnection();
+
+
+
         public frmlogin()
         {
             InitializeComponent();
-        }
 
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C: \Users\ALI\OneDrive\Documents\Visual Studio 2022\login form.accdb
+            Persist security Info=false;";
+        }
         private void btnlogin_Click(object sender, EventArgs e)
         {
-            if(txtuser.Text == "mohammad" && txtpass.Text == "1234")
+            conn.Open();
+                OleDbCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "'select * from Account where username = '" + txtuser.Text + "'and password'" + txtpass.Text + "'";
+            OleDbDataReader or = cmd.ExecuteReader();
+
+            int count = 0;
+            while(or.Read())
             {
-                new frmpos().Show();
-                this.Hide();
-                MessageBox.Show("welcome " + (txtuser.Text));
+                count = count + 1;
+                
+            }
+            if(count == 1)
+            {
+                MessageBox.Show("login successfuly", "success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("the user name or password is incorrect");
+                MessageBox.Show("incorrect username or password", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);          
             }
+            conn.Close();
         }
+
     }
 }
