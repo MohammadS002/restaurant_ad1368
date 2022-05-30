43501929e5227383ff1c42278ced7fac3edf2991
MohammadS002 <101462766+MohammadS002@users.noreply.github.com>
2022-05-30 19:11:00 +0300

    Delete Common7 directory

--- Common7/IDE/SqlWorkbenchProjectItems/Sql/SQL Azure Database/Database/Create Database SqlOnDemand.sql
+++
@@ -1,6 +0,0 @@
--- ===========================================================================
--- Create database template for Azure Synapse SQL Analytics on-demand Database
--- ===========================================================================
-
-CREATE DATABASE <Database_Name, sysname, Database_Name> COLLATE <collation_Name, sysname, SQL_Latin1_General_CP1_CI_AS> 
-GO
